cec4faf4780ebb832c135c47a3340f900de31384
mbrndt <mareike.brandt@hotmail.de>
2024-02-24 16:13:36 +0900

    feat: added landing page

--- mareblog/mareblog/settings.py
+++ mareblog/mareblog/settings.py
@@ -27,6 +27,10 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
+#superuser
+""" mareike1640@gmail.com
+mareblog1! """
+
 
 # Application definition
 
@@ -37,6 +41,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'core.apps.CoreConfig'
 ]
 
 MIDDLEWARE = [
